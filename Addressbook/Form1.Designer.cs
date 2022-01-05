3c5798aa7459150c2fd4d3e02f45f12c09a0a304
Aleksa Kedzic <aleksa.kedzic.17@singimail.rs>
2022-01-05 16:30:16 +0100

    one commit

---
+++ Addressbook/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Addressbook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbPhoneNum = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblPhoneNum = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(140, 34);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(294, 29);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(140, 89);
+            this.tbLastName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(294, 29);
+            this.tbLastName.TabIndex = 0;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(140, 140);
+            this.tbAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(294, 29);
+            this.tbAddress.TabIndex = 0;
+            // 
+            // tbPhoneNum
+            // 
+            this.tbPhoneNum.Location = new System.Drawing.Point(140, 195);
+            this.tbPhoneNum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbPhoneNum.Name = "tbPhoneNum";
+            this.tbPhoneNum.Size = new System.Drawing.Size(294, 29);
+            this.tbPhoneNum.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(19, 37);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(52, 21);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(19, 92);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(81, 21);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last name";
+            // 
+            // lblPhoneNum
+            // 
+            this.lblPhoneNum.AutoSize = true;
+            this.lblPhoneNum.Location = new System.Drawing.Point(19, 198);
+            this.lblPhoneNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhoneNum.Name = "lblPhoneNum";
+            this.lblPhoneNum.Size = new System.Drawing.Size(113, 21);
+            this.lblPhoneNum.TabIndex = 1;
+            this.lblPhoneNum.Text = "Phone number";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(19, 143);
+            this.lblAddress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(66, 21);
+            this.lblAddress.TabIndex = 1;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(19, 250);
+            this.lblGender.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(61, 21);
+            this.lblGender.TabIndex = 1;
+            this.lblGender.Text = "Gender";
+            this.lblGender.Click += new System.EventHandler(this.LblGender_Click);
+            // 
+            // cbGender
+            // 
+            this.cbGender.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cbGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Location = new System.Drawing.Point(140, 247);
+            this.cbGender.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(162, 29);
+            this.cbGender.TabIndex = 2;
+            this.cbGender.SelectedIndexChanged += new System.EventHandler(this.CBoxGender_SelectedIndexChanged);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(468, 89);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(537, 266);
+            this.dgv.TabIndex = 3;
+            this.dgv.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Dgv_MouseDoubleClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSave.Location = new System.Drawing.Point(23, 309);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 46);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnDelete.Location = new System.Drawing.Point(165, 309);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(122, 46);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnReset.Location = new System.Drawing.Point(315, 309);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(119, 46);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(468, 50);
+            this.tbSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(410, 29);
+            this.tbSearch.TabIndex = 0;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(464, 24);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(121, 21);
+            this.lblSearch.TabIndex = 7;
+            this.lblSearch.Text = "Search by name";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSearch.Location = new System.Drawing.Point(899, 33);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(106, 46);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1029, 377);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblPhoneNum);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.tbPhoneNum);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.tbName);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbPhoneNum;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblPhoneNum;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Addressbook/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Addressbook
+{
+    public partial class Form1 : Form
+    {
+        int addressId = 0;
+        //pravi konekciju ka bazi
+        SqlConnection sqlConn = new SqlConnection(@"Data Source=DESKTOP-L7QN8D1;Initial Catalog=Addressbook;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public void openConn() {
+        
+                if (sqlConn.State == ConnectionState.Closed)
+                    sqlConn.Open();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CBoxGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void LblGender_Click(object sender, EventArgs e) {
+               
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = false;
+            openConn();
+            fillDgv();
+            cbGender.Items.Add("muski");
+            cbGender.Items.Add("zenski");
+
+        }
+        //izbacuje tabelu u gridu
+        void fillDgv() {
+            openConn();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("addressVieworSearch", sqlConn);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@name", tbSearch.Text.Trim());
+            DataTable dTable = new DataTable();
+            sqlDa.Fill(dTable);
+            dgv.DataSource = dTable;
+            dgv.Columns[0].Visible = false;
+            sqlConn.Close();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            openConn();
+            SqlCommand sqlCmd = new SqlCommand("addressSaveOrEdit", sqlConn);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            if (btnSave.Text == "Save")
+            {
+                sqlCmd.Parameters.AddWithValue("@mode", "Save");
+                sqlCmd.Parameters.AddWithValue("@address_id", 0);
+                sqlCmd.Parameters.AddWithValue("@name", tbName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@last_name", tbLastName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@phone_number", tbPhoneNum.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@address", tbAddress.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@gender", cbGender.Text);
+                sqlCmd.ExecuteNonQuery();
+                fillDgv();
+                MessageBox.Show("Saved Successfully.");
+                reset();
+            }
+            else {
+                sqlCmd.Parameters.AddWithValue("@mode", "Update");
+                sqlCmd.Parameters.AddWithValue("@address_id", addressId);
+                sqlCmd.Parameters.AddWithValue("@name", tbName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@last_name", tbLastName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@phone_number", tbPhoneNum.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@address", tbAddress.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@gender", cbGender.Text);
+                sqlCmd.ExecuteNonQuery();
+                fillDgv();
+                MessageBox.Show("Updated Successfully.");
+                reset();
+            }
+            sqlConn.Close();
+            
+
+        }
+        //Pretrazuje po imenu
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            openConn();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("addressViewOrSearch", sqlConn);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@name", tbSearch.Text.Trim());
+            DataTable dTable = new DataTable();
+            sqlDa.Fill(dTable);
+            dgv.DataSource = dTable;
+            sqlConn.Close();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+            sqlConn.Close();
+        }
+        //vracamo sve na pocetno stanje
+        public void reset() {
+            tbName.Text = "";
+            tbLastName.Text = "";
+            tbPhoneNum.Text = "";
+            tbAddress.Text = "";
+            cbGender.Text = "";
+            tbSearch.Text = "";
+            addressId = 0;
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            fillDgv();
+            
+        }
+        //kada se klikne dva puta na red u gridu, mozemo da apdejtujemo ili da brisemo red iz tabele
+        private void Dgv_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (dgv.CurrentRow.Index != -1) {
+                tbName.Text = dgv.CurrentRow.Cells[1].Value.ToString();
+                tbLastName.Text = dgv.CurrentRow.Cells[2].Value.ToString();
+                tbPhoneNum.Text = dgv.CurrentRow.Cells[3].Value.ToString();
+                tbAddress.Text = dgv.CurrentRow.Cells[4].Value.ToString();
+                cbGender.Text = dgv.CurrentRow.Cells[5].Value.ToString(); 
+            }
+            addressId = Convert.ToInt32(dgv.CurrentRow.Cells[0].Value);
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            openConn();
+            SqlCommand sqlCmd = new SqlCommand("deleteRow", sqlConn);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@address_id", addressId);
+            sqlCmd.ExecuteNonQuery();
+            MessageBox.Show("Row Successfully Deleted.");
+            reset();
+            fillDgv();
+            sqlConn.Close();
+
+
+        }
+    }
+}
